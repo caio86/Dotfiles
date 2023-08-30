09da728c79e1638b2d8e98415acf58a6faf66a5b
caio86 <80562020+caio86@users.noreply.github.com>
2023-08-30 09:05:59 -0300

    edit plugins.lua

--- .config/nvim/lua/c410/plugins.lua
+++ .config/nvim/lua/c410/plugins.lua
@@ -1,57 +1,60 @@
 local M = {
-    { "catppuccin/nvim",   name = "catppuccin", priority = 1000 },
-    {
-        'nvim-telescope/telescope.nvim',
-        lazy = true,
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-    },
-    { 'tpope/vim-fugitive' },
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        dependencies = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },             -- Required
-            { 'williamboman/mason.nvim' },           -- Optional
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"nvim-telescope/telescope.nvim",
+		lazy = true,
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{ "tpope/vim-fugitive" },
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ "williamboman/mason.nvim" }, -- Optional
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },     -- Required
-        }
-    },
-    {
-        'lewis6991/gitsigns.nvim',
-    },
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-    },
-    {
-        'akinsho/bufferline.nvim',
-        version = "*",
-        dependencies = 'nvim-tree/nvim-web-devicons'
-    },
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
-    },
-    { "lukas-reineke/indent-blankline.nvim" },
-    { "HiPhish/nvim-ts-rainbow2" },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{
+				"L3MON4D3/LuaSnip", -- Required
+				dependencies = { "rafamadriz/friendly-snippets" },
+			},
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+	},
+	{ "lukas-reineke/indent-blankline.nvim" },
+	{ "HiPhish/nvim-ts-rainbow2" },
 }
 
 return M
